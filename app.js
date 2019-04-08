b8bbc691c5b1dae94130f105a5f642af9090c25c
Jake Holtz <jahholtz@gmail.com>
2019-04-08 13:13:43 -0700

    Added basic styles, connected angularJS

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ const path = require('path');
 /* Untracked file thats stores API authentication information */
 const { SHOPIFY } = require('./config/auth.js');
 
+/* Use client directory */
+app.use(express.static('client'));
+app.use('/client', express.static(__dirname + '/client'));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 /* Gets List of Items from Shopify */
 const getShopifyItems = (SHOPIFY) => {
   request(`${SHOPIFY.API_URL}/products.json`, (error, response, items) => {
-    console.log(items);
+    console.log(JSON.parse(items));
   });
 }
 
