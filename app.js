c5470e64c0744ea187c38402519cafea1a193029
Jake Holtz <jahholtz@gmail.com>
2019-04-07 21:16:03 -0700

    Successfully connecting to shopify

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const app = express();
 const request = require('request');
 const path = require('path');
 
+/* Untracked file thats stores API authentication information */
+const { SHOPIFY } = require('./config/auth.js');
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/index.html'));
 });
+
+/* Gets List of Items from Shopify */
+const getShopifyItems = (SHOPIFY) => {
+  request(`${SHOPIFY.API_URL}/products.json`, (error, response, items) => {
+    console.log(items);
+  });
+}
 
 app.listen(3030, () => `Listening in on port 3030`);
