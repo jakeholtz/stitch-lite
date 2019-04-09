94604df0c708e302195c43cb00a8ebdbfa427783
Jake Holtz <jahholtz@gmail.com>
2019-04-08 18:35:55 -0700

    Added API endpoints at api/sync, api/products, api/products/:productId

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/* TO DO 
+  - Connect Vend
+  - Dynamically update DB / inventory only based off of updated date
+  - Reformulate from callbacks to promises
+  - Reformulate from raw SQL to ORM
+*/
+
 const express = require('express');
 const app = express();
 
@@ -5,6 +12,7 @@ const request = require('request');
 const path = require('path');
 const db = require('./db/database.js');
 const { SHOPIFY } = require('./config/auth.js');
+const { createConnection, createProducts, selectProducts } = db;
 
 /* Use client directory */
 app.use(express.static('client'));
@@ -17,32 +25,26 @@ app.get('/', (req, res) => {
 
 /* Get updated products and send to AngularJS FE */
 app.get('/products', (req, res) => {
-  const { createConnection, initiateDB, selectProducts, insertProducts } = db;
   const connection = createConnection();
 
   connection.connect((err) => {
     if (err) throw err;
 
-    /* Initiate seed data (databases and table) if doesn't exist */
-    connection.query(initiateDB, (err, result) => { if (err) throw err; });
-
     /* get shopify information */
     request(`${SHOPIFY.API_URL}/products.json`, (error, response, data) => {
       var shopifyData = parseShopifyItems(data);
 
-      /* Update DB with shopify, get DB products */
-      connection.query(insertProducts(shopifyData), function (err) {
+      /* Initiate seed data (databases and table) if doesn't exist */
+      connection.query(createProducts(shopifyData), (err, result) => {
         if (err) throw err;
 
-        connection.query(selectProducts, function (err, result) {
+        connection.query(selectProducts(), function (err, result) {
           if (err) throw err;
-          const [request, data] = result;
-  
-          res.send(data);
+          res.send(formatResult(result));
         });
+
       });
     });
-
   });
 });
 
@@ -63,5 +65,44 @@ function parseShopifyItems(data) {
   })
   return items;
 }
+
+function formatResult(result) {
+  const [info, data] = result;
+  return data;
+}
+
+/* API endpoints  */
+app.get('/api/products', (req, res) => {
+  createConnection().query(selectProducts(), function (err, result) {
+    if (err) throw err;
+    res.send(formatResult(result));
+  });
+});
+
+app.get('/api/products/:productId', (req, res) => {
+  const { productId } = req.params;
+
+  createConnection().query(selectProducts('id', productId), function (err, result) {
+    if (err) throw err;
+    res.send(formatResult(result));
+  });
+});
+
+app.post('/api/sync', (req, res) => {
+  request(`${SHOPIFY.API_URL}/products.json`, (error, response, data) => {
+    const shopifyData = parseShopifyItems(data);
+    const connection = createConnection();
+
+    connection.query(createProducts(shopifyData), (err, result) => {
+      if (err) throw err;
+
+      connection.query(selectProducts(), function (err, result) {
+        if (err) throw err;
+        res.send('Products updated successfully');
+      });
+    });
+  });
+});
+
 
 app.listen(3030, () => `Listening in on port 3030`);
--- db/database.js
+++ db/database.js
@@ -11,34 +11,40 @@ function createConnection() {
   });
 }
 
-const initiateDB =
- `CREATE DATABASE IF NOT EXISTS stitch_lite;
-  USE stitch_lite;
-  CREATE TABLE IF NOT EXISTS products (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    variant VARCHAR(255) NOT NULL,
-    sku VARCHAR(32) NOT NULL,
-    quantity INT NOT NULL,
-    updated_at TIMESTAMP
-  );`;
-
-const selectProducts = `${baseQuery} SELECT * FROM products`
+const selectProducts = (column, value) => {
+  let query = `${baseQuery} SELECT * FROM products`;
+  if (!column || !value) return `${query};`;
+  return `${query} WHERE ${column} = ${value};`;
+}
 
 const insertProducts = (data) => {
   let query = baseQuery;
   data.forEach(row => {
-    let { name, variant, quantity, sku } = row;
+    let { name, variant, quantity, sku, updated_at } = row;
+    updated_at = new Date(updated_at).toISOString().slice(0, 19).replace('T', ' ');
     query +=
-     `INSERT INTO products (name, variant, sku, quantity)
-      VALUES ('${name}', '${variant}', '${sku}', '${quantity}'); `
+     `REPLACE INTO products (name, variant, sku, quantity, updated_at)
+      VALUES ('${name}', '${variant}', '${sku}', '${quantity}', '${updated_at}'); `
   })
   return query;
 }
-  
 
+const createProducts = (data) => {
+  let query = `CREATE DATABASE IF NOT EXISTS stitch_lite;
+    USE stitch_lite;
+    CREATE TABLE IF NOT EXISTS products (
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      variant VARCHAR(255) NOT NULL,
+      sku VARCHAR(32) NOT NULL UNIQUE,
+      quantity INT NOT NULL,
+      updated_at TIMESTAMP
+    ); `;
+    if (data) query += insertProducts(data);
+    return query;
+};
 
-module.exports = { createConnection, initiateDB, selectProducts, insertProducts };
+module.exports = { createConnection, createProducts, selectProducts };
 
 
 
